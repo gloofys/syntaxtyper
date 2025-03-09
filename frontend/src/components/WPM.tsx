ccef479cd437fd13426c7c86980ec68eb289af19
fredb <fred.brosman06@gmail.com>
2025-03-09 20:39:11 +0200

    Changed Words per minute to characters per minute.

--- frontend/src/components/WPM.tsx
+++ frontend/src/components/WPM.tsx
@@ -6,11 +6,10 @@ interface WPMProps {
 }
 
 const WPM: React.FC<WPMProps> = ({ time, charCount}) => {
-    const wordsTyped = charCount / 5;
     const minutes = time/60;
-    const wpm = minutes > 0 ? Math.round(wordsTyped / minutes) : 0;
+    const cpm = minutes > 0 ? Math.round(charCount / minutes) : 0;
 
-    return <div className="text-xl font-bold">WPM: {wpm}</div>;
+    return <div className="text-xl font-bold">Characters per minute: {cpm}</div>;
 }
 
 export default WPM;
